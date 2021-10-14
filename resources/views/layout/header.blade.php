f3898eabea2419ceb4029f9e03d1cafbab2bd4e5
sabreenMohammed107 <sabreenm312@gmail.com>
2021-10-14 09:06:00 +0200

    test

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -40,14 +40,14 @@
                             </a>
                         </li>
                         @endcan
-                        @can('roles-list')
+                        {{-- @can('roles-list')
                         <li class="nav-item">
                             <a href="{{ route('roles.index') }}" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p> الأدوار </p>
                             </a>
                         </li>
-                        @endcan
+                        @endcan --}}
                         @can('units-list')
                         <li class="nav-item">
                             <a href="{{ route('units.index') }}" class="nav-link">
